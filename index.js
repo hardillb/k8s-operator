5518258f1303944e55fb0861f0676272066f9b6a
Ben Hardill <hardillb@gmail.com>
2024-12-16 21:12:37 +0000

    First pod

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const { CustomObjectsApi, KubeConfig, Watch } = require('@kubernetes/client-node')
+const { AppsV1Api, CoreV1Api, CustomObjectsApi, KubeConfig, NetworkingV1Api, Watch } = require('@kubernetes/client-node')
+
+const { podTemplate } = require('./lib/templates.js')
 
 const group = 'k8s.hardill.me.uk'
 const version = 'v1'
 const plural = 'randoms'
 
 const kubeConfig = new KubeConfig();
-kubeConfig.loadFromDefault();
+// kubeConfig.loadFromDefault();
+kubeConfig.loadFromFile('/etc/rancher/k3s/k3s.yaml')
 const watch = new Watch(kubeConfig)
 
+const k8sApi = kubeConfig.makeApiClient(CoreV1Api)
+const k8sAppApi = kubeConfig.makeApiClient(AppsV1Api)
+const k8sNetApi = kubeConfig.makeApiClient(NetworkingV1Api)
+
+
+
 async function watching () {
-    const req = await watch.watch('/apis/k8s.hardill.me.uk/v1/namespaces/*/randoms',
-        {},
+    const req = await watch.watch('/apis/k8s.hardill.me.uk/v1/namespaces//randoms',
+        {
+            // allowWatchBookmarks: true
+        },
         async (type, api, obj) => {
             switch (type) {
                 case 'ADDED':
+                    // create pod
+                    const localPod = JSON.parse(JSON.stringify(podTemplate))
+                    localPod.metaData.name = `${api.metaData.name}-node-red`
+                    console.log(JSON.stringify(localPod, null, 2))
+                    k8sApi.createNamespacedPod(api.metaData.namespace, localPod)
                 case 'MODIFIED':
                 case 'DELETED':
+                    // delete pod
             }
             console.log(type)
-            console.log(api)
+            // console.log(JSON.stringify(api, null, 2))
         },
         (err) => {
             console.log(err)
